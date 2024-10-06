22c41122970e0c8263a6ce37e4bb53e04d5ca104
Daniel <leitepereira.daniel2005@gmail.com>
2024-10-06 15:14:38 -0300

    feat: add global component and styles

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Footer } from "./components/Footer"
+import { Landing } from "./pages/Landing"
+
+function App() {
+
+  return (
+    <Landing />
+    // <EventosPassados />
+    // <Footer />
+  )
+}
+
+export default App
