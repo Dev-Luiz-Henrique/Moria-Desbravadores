04e070626c5c14f84656cb323bce850bbdb9792a
Daniel <leitepereira.daniel2005@gmail.com>
2024-10-06 15:16:47 -0300

    feat: add landing page and styles

---
+++ front-end/src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/header";
+
+export function Landing() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    )
+}
