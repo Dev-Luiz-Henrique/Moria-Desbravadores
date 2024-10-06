56ebb702f7c248150f8fc9178c5d8e0edb48f824
Daniel <leitepereira.daniel2005@gmail.com>
2024-10-06 15:15:41 -0300

    feat: add footer component and styles

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import FacebookLogo from "../assets/img/Facebook.png"
+import YoutubeLogo from "../assets/img/Youtube.png"
+import InstagramLogo from "../assets/img/Instagram.png"
+import "./Footer.css"
+
+export function Footer() {
+    return(
+        <footer className="footer">
+            <p>Associação Paulistana - UCB</p>
+            <p>Fundado em 1982</p>
+            <span>
+                <a href=""><img src={InstagramLogo} alt="" /></a>
+                <a href=""><img src={FacebookLogo} alt="" /></a>
+                <a href=""><img src={YoutubeLogo} alt="" /></a>
+            </span>
+        </footer>
+    )
+}
