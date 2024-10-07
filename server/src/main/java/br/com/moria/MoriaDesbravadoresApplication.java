537e84565201e654a8ee415575414f36253a08f9
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-07 19:52:31 -0300

    feat: initialize Spring Boot API project with Maven

---
+++ server/src/main/java/br/com/moria/MoriaDesbravadoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.moria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoriaDesbravadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoriaDesbravadoresApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/moria/MoriaDesbravadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoriaDesbravadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
