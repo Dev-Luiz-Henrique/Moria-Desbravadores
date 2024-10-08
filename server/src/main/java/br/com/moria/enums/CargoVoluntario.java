5191f177d54feebf2a4afbc0c5cb51e758893b3f
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-07 23:07:05 -0300

    feat: add Enum for CargoVoluntario

---
+++ server/src/main/java/br/com/moria/enums/CargoVoluntario.java
@@ -0,0 +1,11 @@
+package br.com.moria.enums;
+
+public enum CargoVoluntario {
+    DIRETOR_CLUBE,
+    DIRETOR_ASSOCIADO,
+    TESOUREIRO,
+    CAPELAO,
+    SECRETARIO,
+    INSTRUTOR_CLASSES_PROGRESSIVAS,
+    CONSELHEIRO_UNIDADE;
+}
