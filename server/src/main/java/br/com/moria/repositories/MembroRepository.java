b8ab3b6717d77a2988f9176a3d506fc44d7cdccd
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-08 22:40:42 -0300

    feat: create MembroRepository for Membro entity

---
+++ server/src/main/java/br/com/moria/repositories/MembroRepository.java
@@ -0,0 +1,9 @@
+package br.com.moria.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.moria.models.Membro;
+
+public interface MembroRepository extends JpaRepository<Membro, Long> {
+
+}
