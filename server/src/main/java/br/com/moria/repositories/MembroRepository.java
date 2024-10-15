eab10ba231308a58671e1fdce10fc2400a8438b3
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-14 21:52:55 -0300

    feat: add methods findByEmail and findByCpf in MembroRepository

--- server/src/main/java/br/com/moria/repositories/MembroRepository.java
+++ server/src/main/java/br/com/moria/repositories/MembroRepository.java
@@ -1,9 +1,13 @@
 package br.com.moria.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.moria.models.Membro;
 
 public interface MembroRepository extends JpaRepository<Membro, Integer> {
 
+    Optional<Membro> findByEmail(String email);
+    Optional<Membro> findByCpf(String cpf);
 }
