a6bc90c7531d28f5641130e41af9cd47c7dcc02e
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-20 20:52:16 -0300

    feat: implement JWT login endpoint and AuthRequest class

---
+++ server/src/main/java/br/com/moria/controllers/AuthController.java
@@ -0,0 +1,49 @@
+package br.com.moria.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import br.com.moria.configurations.JwtUtil;
+import br.com.moria.services.implementations.MembroDetailsServiceImpl;
+import lombok.Getter;
+import lombok.Setter;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MembroDetailsServiceImpl membroDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
+            );
+        } catch (Exception e) {
+            throw new Exception("Usuário ou senha inválidos", e);
+        }
+
+        final UserDetails userDetails = membroDetailsService.loadUserByUsername(authRequest.getEmail());
+        
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return jwt;
+    }
+}
+
+@Getter
+@Setter
+class AuthRequest {
+    private String email;
+    private String password;
+}
