4f0983912590f1c0bdbaf6bf902712a975e922e8
Daniel <leitepereira.daniel2005@gmail.com>
2024-10-23 17:19:05 -0300

    feat: add user management page

--- front-end/src/components/UserPage.jsx
+++ front-end/src/components/UserPage.jsx
@@ -1,31 +1,44 @@
 import { CardUser } from "./CardUser"
 import "./UserPage.css"
+import AddIcon from "../assets/img/Plus.svg"
+import SearchIcon from "../assets/img/Glass.svg"
+import { HeaderSignedIn } from "./HeaderSignedIn"
+import { Footer } from "./Footer"
 
 export function UserPage() {
-    return(
-        <section className="container-user-page">
-            <div className="filter">
-                <div className="detail">
-                    <button>
-                        <img src="" alt="" />
-                        <p>CADASTRAR MEMBRO</p>
-                    </button>
-                    <div className="search"></div>
-                    <div className="filter-users">
+    return (
+        <>
+            <HeaderSignedIn />
+            <section className="container-user-page">
+                <div className="filter">
+                    <div className="detail">
+                        <button>
+                            <img src={AddIcon} alt="" />
+                            <p>CADASTRAR MEMBRO</p>
+                        </button>
+                        <div className="search">
+                            <img src={SearchIcon} alt="" />
+                            <input type="text" />
+                        </div>
+                        {/* <div className="filter-users">
                         <span>
                             <img src="" alt="" />
                             <p>FILTROS</p>
-                        </span>                        
+                        </span>
+                    </div> */}
                     </div>
                 </div>
-            </div>
 
-            <div className="users">
-                <CardUser />
-                <CardUser />
-                <CardUser />
-                <CardUser />
-            </div>
-        </section>
+                <div className="users-space">
+                    <div className="users">
+                        <CardUser />
+                        <CardUser />
+                        <CardUser />
+                        <CardUser />
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
     )
 }
