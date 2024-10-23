164aeca9cdb31f17241dee056bfa63183b319ab1
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-23 15:10:29 -0300

    feat: update login screen to handle API login
    - Integrated API call for user authentication
    - Added email and password validation
    - Implemented navigation to dashboard upon successful login
    - Improved error handling for login process

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,32 +1,18 @@
-import "./App.css"
-import { ButtonGoBack } from "./components/ButtonGoBack"
-import { CardUser } from "./components/CardUser"
-import { DropDownProfile } from "./components/DropDownProfile"
-import { Footer } from "./components/Footer"
-import { HeaderNoNav } from "./components/HeaderNoNav"
-import { HeaderSignedIn } from "./components/HeaderSignedIn"
-import { LoginBody } from "./components/LoginBody"
-import { PastEvents } from "./components/PastEvents"
-import { SectionEvents } from "./components/SectionEvents"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Landing } from "./pages/Landing"
 import { LoginPage } from "./pages/LoginPage"
+import "./App.css"
 
 function App() {
 
   return (
-    // <Landing />
-    // <HeaderSignedIn />
-    // <LoginBody />
-    // <HeaderNoNav />
-    // <LoginPage /> 
-    <CardUser />
-    // <ButtonGoBack />
-    // <DropDownProfile />
-    // <EventosPassados />
-    // <Footer />
-    // <SectionEvents />
-    // <PastEvents />
+    <Router>
+        <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<LoginPage />} />
+        </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
