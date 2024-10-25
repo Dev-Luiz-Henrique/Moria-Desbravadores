83f9fa5059ab6710677c8d64d05f5b4b41c0f0d5
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-24 21:33:14 -0300

    feat: add authorities to JWT claims

--- server/src/main/java/br/com/moria/configurations/JwtUtil.java
+++ server/src/main/java/br/com/moria/configurations/JwtUtil.java
@@ -42,6 +42,7 @@ public class JwtUtil {
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("authorities", userDetails.getAuthorities());
         return createToken(claims, userDetails.getUsername());
     }
 
