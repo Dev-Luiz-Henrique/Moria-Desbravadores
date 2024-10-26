14cf5ea01ff044da2a21006032b04e48a556315e
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-25 23:24:40 -0300

    feat: add downloadFile functions and rename UploadService to FileService

--- server/src/main/java/br/com/moria/services/implementations/FileServiceImpl.java
+++ server/src/main/java/br/com/moria/services/implementations/FileServiceImpl.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.moria.services.interfaces.IUploadService;
+import br.com.moria.dtos.FileResponseDTO;
+import br.com.moria.services.interfaces.IFileService;
 
 @Service
-public class UploadServiceImpl implements IUploadService {
+public class FileServiceImpl implements IFileService {
 
     private String uploadBaseDir = "/uploads";
 
@@ -35,18 +36,30 @@ public class UploadServiceImpl implements IUploadService {
         Path filePath = uploadPath.resolve(uniqueFileName);
 
         String contentType = file.getContentType();
-        if (!"image/png".equals(contentType) && !"image/jpeg".equals(contentType)) {
+        if (!"image/png".equals(contentType) && !"image/jpeg".equals(contentType))
 			throw new IllegalArgumentException("Tipo de arquivo não permitido. Formatos aceitos: jpeg e png.");
-		}
 
-        if (file.getSize() > 5 * 1024 * 1024) { // 5MB
+        if (file.getSize() > 5 * 1024 * 1024) // 5MB
 			throw new IllegalArgumentException("O tamanho do arquivo deve ser inferior a 5MB.");
-		}
 
         Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
-        Path relativePath = Paths.get(uploadDir, fileName);
+        Path relativePath = Paths.get(uploadDir, uniqueFileName);
         return relativePath.toString();
+    }
+
+    private byte[] downloadFile(String filePath) throws IOException {
+        Path fullPath = Paths.get(System.getProperty("user.dir"), uploadBaseDir, filePath);
+        System.out.println(fullPath);
+    
+        if (!Files.exists(fullPath))
+            throw new IOException("Arquivo não encontrado");
+        return Files.readAllBytes(fullPath);
+    }
+
+    private String getContentType(String filePath) throws IOException {
+        Path fullPath = Paths.get(System.getProperty("user.dir"), uploadBaseDir, filePath);
+        return Files.probeContentType(fullPath);
     }
 
     @Override
@@ -63,4 +76,9 @@ public class UploadServiceImpl implements IUploadService {
 	public String uploadAutorizacaoResponsavel(MultipartFile file) throws IOException {
 		return uploadFile(file, "autorizacaoResponsavel");
 	}
+
+    @Override
+    public FileResponseDTO downloadFichaSaude(String filePath) throws IOException {
+        return new FileResponseDTO(downloadFile(filePath), getContentType(filePath));
+    }
 }
--- server/src/main/java/br/com/moria/services/interfaces/IFileService.java
+++ server/src/main/java/br/com/moria/services/interfaces/IFileService.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public interface IUploadService {
+import br.com.moria.dtos.FileResponseDTO;
 
+public interface IFileService {
     public String uploadFichaSaude(MultipartFile file) throws IOException;
     public String uploadComprovantePagamento(MultipartFile file) throws IOException;
     public String uploadAutorizacaoResponsavel(MultipartFile file) throws IOException;
+    public FileResponseDTO downloadFichaSaude(String filePath) throws IOException;
 }
