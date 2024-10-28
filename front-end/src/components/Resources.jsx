0cc0c2af4b11a1c5c7e1b588cf9294c0bd620c57
Diego <odiegodev@gmail.com>
2024-10-28 05:35:35 -0300

    feat: add Resources component and style

---
+++ front-end/src/components/Resources.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import DeleteImg from '../assets/img/Delete.svg';
+import PlusImg from '../assets/img/Plus.svg';
+import './Resources.css';
+
+const data = [
+  {
+    nome: 'cadeira',
+    valor: '50,00',
+    quantidade: '250',
+    categoria: 'móvel',
+    descrição: 'cadeira de plástico',
+  },
+  {
+    nome: 'copo',
+    valor: '5,00',
+    quantidade: '50',
+    categoria: 'descartável',
+    descrição: 'conjunto de copos de plástico',
+  },
+];
+
+export function Resources() {
+  return (
+    <section>
+      <header className="resources-header">
+        {/* TODO: Esse Botão só será exibido para quem tiver a autorização adequada */}
+        <button className="adicionar" type="button">
+          <img src={PlusImg} alt="" />
+          ADICIONAR RECURSO
+        </button>
+        <h3>Recursos</h3>
+        {/* TODO: Esse Botão só será exibido para quem tiver a autorização adequada */}
+        <button className="salvar" type="button">
+          SALVAR
+        </button>
+      </header>
+      <ul className="resourcesList">
+        <li className="list-header">
+          <span>NOME</span>
+          <span>VALOR</span>
+          <span>QUANTIDADE</span>
+          <span>CATEGORIA</span>
+          <span>DESCRIÇÃO</span>
+        </li>
+        {data.map((inscrito) => {
+          return (
+            // Se esse objeto tiver uma ID, será uma key melhor
+            <li key={inscrito.nome}>
+              {/* TODO: Esse Botão só será exibido para quem tiver a autorização adequada */}
+              <button type="button">
+                <img src={DeleteImg} alt="Deletar" />
+              </button>
+              <span>{inscrito.nome}</span>
+              <span>R$ {inscrito.valor}</span>
+              <span>{inscrito.quantidade} un</span>
+              <span>{inscrito.categoria}</span>
+              <span>{inscrito.descrição}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
