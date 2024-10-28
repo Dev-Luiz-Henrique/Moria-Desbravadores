e4da19aa31d001d15e383cf100718fd2c61bb6c6
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-28 12:49:04 -0300

    feat: add basic function to formatPhoneNumber

--- front-end/src/utils/stringHelpers.jsx
+++ front-end/src/utils/stringHelpers.jsx
@@ -11,7 +11,29 @@ export function abbreviateName(name) {
     return `${firstName} ${middleInitials} ${lastName}`;
 }
 
+export function normalizeUnderscore(role) {
+    return role.toLowerCase().split('_')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+// Formatação de dados do membro
+
 export function formatEndereco(endereco) {
     const { logradouro, numero, bairro, cidade, estado } = endereco;
     return `${logradouro}, ${numero} - ${bairro}, ${cidade} - ${estado}`;
 }
+
+export function formatPhoneNumber(number) {
+    const cleaned = number.replace(/\D/g, "");
+    return cleaned.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+};
+
+export function cleanPhoneNumber (number) {
+    number.replace(/\D/g, "");
+} 
+
+export const memberUtils = {
+    formatEndereco,
+    formatPhoneNumber,
+    cleanPhoneNumber
+};
