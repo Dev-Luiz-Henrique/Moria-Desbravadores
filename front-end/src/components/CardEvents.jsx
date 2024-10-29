f26cb93638739f4e3d22764c6e3380aef22a0faa
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2024-10-29 01:15:43 -0300

    feat: implement api connection to get eventos

--- front-end/src/components/CardEvents.jsx
+++ front-end/src/components/CardEvents.jsx
@@ -1,39 +1,41 @@
 import DefaultEvent from "../assets/img/default-event.png"
 import Delete from "../assets/img/Delete.svg"
 import Edit from "../assets/img/Edit.svg"
+import { formatDateWithHours } from "../utils/dateHelpers";
+import { formatEndereco } from "../utils/stringHelpers";
 import "./CardEvents.css"
 
-export function CardEvents() {
+export function CardEvents({ nome, endereco, logradouro, numero, dataInicio, descricao, atracao, onDelete }) {
     return (
         <div className="card-event-container">
-            <img src={DefaultEvent} alt="" />
+            <img src={DefaultEvent} alt={nome} />
             <div className="card-events-buttons">
-                <button><img src={Edit} alt="" /></button>
-                <button><img src={Delete} alt="" /></button>
+                <button><img src={Edit} alt="Edit" /></button>
+                <button onClick={onDelete}><img src={Delete} alt="Delete" /></button>
             </div>
-            <div className="card-event-text">
-                <h3>CAMPORI 2024</h3>
 
+            <div className="card-event-text">
+                <h3>{nome}</h3>
                 <span>
                     <b>ENDEREÇO:</b>
-                    <p>IABC Planalmira - GO</p>
+                    <p>{formatEndereco(endereco, logradouro, numero)}</p>
                 </span>
 
                 <span>
                     <b>DATA:</b>
-                    <p>24-28 de Julho</p>
+                    <p>{formatDateWithHours(dataInicio)}</p>
                 </span>
 
                 <span>
                     <b>DESCRIÇÃO:</b>
-                    <p>Reunião anual do clube dos Desbravadores</p>
+                    <p>{descricao}</p>
                 </span>
 
                 <span>
                     <b>ATRAÇÃO:</b>
-                    <p>---</p>
+                    <p>{atracao}</p>
                 </span>
             </div>
         </div>
-    )
+    );
 }
--- front-end/src/pages/ManageEvents.jsx
+++ front-end/src/pages/ManageEvents.jsx
@@ -1,4 +1,7 @@
-import { PastEvents } from "../components/PastEvents";
+import { useAuth } from "../context/AuthContext";
+import { useFetch } from "../hooks/useFetch";
+import { apiRequest } from "../utils/api";
+import { Authorities } from "../utils/authorities";
 import AddIcon from "../assets/img/Plus.svg";
 import SearchIcon from "../assets/img/Glass.svg";
 import "./ManageEvents.css"
@@ -7,6 +10,30 @@ import { Header } from "../components/header";
 import { Footer } from "../components/Footer";
 
 export function ManageEvents() {
+    const { authorities } = useAuth();
+    const allowedAuthorities = [Authorities.DIRETOR_CLUBE, Authorities.DIRETOR_ASSOCIADO, Authorities.SECRETARIO];
+    const hasAccess = allowedAuthorities.some(auth => authorities.includes(auth));
+
+    const { data: eventos, setData: setEventos, loading, error } = useFetch("/eventos", "GET");
+
+    const handleDelete = async (id) => {
+        if (!hasAccess) {
+            alert("Você não tem permissão para excluir membros.");
+            return;
+        }
+
+        const confirmed = window.confirm("Você tem certeza que deseja excluir este membro?");
+        if (confirmed) {
+            const { error: deleteError } = await apiRequest(`/eventos/${id}`, "DELETE");
+
+            if (deleteError)
+                console.error(deleteError);
+            else 
+                setEventos((prevEventos) => prevEventos.filter(evento => evento.id !== id))
+        }
+    };
+
+
     return (
         <>
             <Header />
@@ -26,9 +53,19 @@ export function ManageEvents() {
 
                 <div className="events-space">
                     <div className="events">
-                        <CardEvents />
-                        <CardEvents />
-                        <CardEvents />
+                        {!error && !loading && eventos.map((evento) => (
+                            <CardEvents 
+                                key={evento.id}
+                                nome={evento.nome}
+                                endereco={evento.endereco}
+                                logradouro={evento.logradouro}
+                                numero={evento.numero}
+                                dataInicio={evento.dataInicio}
+                                descricao={evento.descricao}
+                                atracao={evento.atracao}
+                                onDelete={() => handleDelete(eventos.id)}
+                            />
+                        ))}
                     </div>
                 </div>
             </section>
